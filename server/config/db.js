8bad609016063e1018a5b23d5d85aa49e5666daa
SumanKarmakar467 <karmakarsuman12138@gmail.com>
2026-05-17 12:00:52 +0530

    chore: enhance MongoDB connection error handling and server startup

--- server/config/db.js
+++ server/config/db.js
@@ -7,8 +7,26 @@ const connectDB = async () => {
     throw new Error("MONGO_URI is not defined in environment variables");
   }
 
-  const conn = await mongoose.connect(uri);
-  console.log(`MongoDB connected: ${conn.connection.host}`);
+  try {
+    console.log("Connecting to MongoDB...");
+
+    const conn = await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 8000,
+      connectTimeoutMS: 8000
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    const details = [
+      "Unable to connect to MongoDB Atlas.",
+      "Check that your current public IP is allowed in Atlas Network Access.",
+      "Also verify that your MongoDB username/password are correct and URL-encoded in MONGO_URI.",
+      "If you are on a restricted network, allow outbound TCP traffic to port 27017 or try another network/VPN."
+    ];
+
+    error.message = `${details.join(" ")} Original error: ${error.message}`;
+    throw error;
+  }
 };
 
 export default connectDB;
